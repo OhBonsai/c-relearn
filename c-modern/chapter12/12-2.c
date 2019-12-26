fe7e3953f1d7b1bc60f35d7fad747019c3d95b76
penzai <penzai@fordeal.com>
2019-12-26 22:21:20 +0800

    chapter 12

---
+++ c-modern/chapter12/12-2.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+
+void reserve_words(const char *a, char *b) {
+	int len = strlen(a);
+
+	for (int i=0; i<len; i++) {
+		*(b+len-i-1) = *(a+i);
+	}
+	
+}
+
+
+
+int main(void) {
+	char *a, *b;
+	_Bool yes = true;
+
+	printf("ENTER MESSAGE: ");
+	scanf("%s", a);
+
+	int len = strlen(a);
+
+	for(int i=0; i<len ;i++) {
+		if ( *(a+i) != *(b+i)) {
+			yes = false;
+			
+		}
+	
+	}
+
+	printf("Enter %d", yes);
+
+
+
+	return 0;
+}
